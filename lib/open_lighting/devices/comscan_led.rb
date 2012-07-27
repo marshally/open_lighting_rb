6445e9abafdf3f84374c32c9e94e9e6b2fe08cc2
Marshall Yount <myount@zynga.com>
2012-07-26 23:39:29 -0500

    more named points for comscan led

--- lib/open_lighting/devices/comscan_led.rb
+++ lib/open_lighting/devices/comscan_led.rb
@@ -8,12 +8,15 @@ module OpenLighting
         options[:points] ||= {}
         options[:points][:center] ||= {:pan => 127, :tilt => 127}
 
+        options[:points][:strobe_blackout]  ||= {:strobe => 0}
+        options[:points][:strobe_open]      ||= {:strobe => 8}
         options[:points][:strobe_slow]      ||= {:strobe => 16}
         options[:points][:strobe_fast]      ||= {:strobe => 131}
         options[:points][:strobe_slow_fast] ||= {:strobe => 140}
         options[:points][:strobe_fast_slow] ||= {:strobe => 190}
         options[:points][:strobe_random]    ||= {:strobe => 247}
 
+        options[:points][:nocolor]  ||= {:gobo => 0}
         options[:points][:yellow]   ||= {:gobo => 8}
         options[:points][:red]      ||= {:gobo => 15}
         options[:points][:green]    ||= {:gobo => 22}
@@ -22,6 +25,9 @@ module OpenLighting
         options[:points][:polka]    ||= {:gobo => 43}
         options[:points][:teal]     ||= {:gobo => 50}
         options[:points][:rings]    ||= {:gobo => 57}
+
+        options[:points][:on]       ||= {:dimmer => 255}
+        options[:points][:off]      ||= {:dimmer => 255}
 
         options[:capabilities] ||= [:pan, :tilt, :strobe, :gobo, :dimmer]
 
