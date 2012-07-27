8fc0fba17e6e85c12729f59ecb331532e7f2aaa7
Marshall Yount <myount@zynga.com>
2012-07-26 21:30:28 -0500

    formatting and alignment

--- lib/open_lighting/devices/comscan_led.rb
+++ lib/open_lighting/devices/comscan_led.rb
@@ -8,20 +8,20 @@ module OpenLighting
         options[:points] ||= {}
         options[:points][:center] ||= {:pan => 127, :tilt => 127}
 
-        options[:points][:strobe_slow] ||= {:strobe => 16}
-        options[:points][:strobe_fast] ||= {:strobe => 131}
+        options[:points][:strobe_slow]      ||= {:strobe => 16}
+        options[:points][:strobe_fast]      ||= {:strobe => 131}
         options[:points][:strobe_slow_fast] ||= {:strobe => 140}
         options[:points][:strobe_fast_slow] ||= {:strobe => 190}
-        options[:points][:strobe_random] ||= {:strobe => 247}
+        options[:points][:strobe_random]    ||= {:strobe => 247}
 
-        options[:points][:yellow] ||= {:gobo => 8}
-        options[:points][:red] ||= {:gobo => 15}
-        options[:points][:green] ||= {:gobo => 22}
-        options[:points][:blue] ||= {:gobo => 29}
+        options[:points][:yellow]   ||= {:gobo => 8}
+        options[:points][:red]      ||= {:gobo => 15}
+        options[:points][:green]    ||= {:gobo => 22}
+        options[:points][:blue]     ||= {:gobo => 29}
         options[:points][:teardrop] ||= {:gobo => 36}
-        options[:points][:polka] ||= {:gobo => 43}
-        options[:points][:teal] ||= {:gobo => 50}
-        options[:points][:rings] ||= {:gobo => 57}
+        options[:points][:polka]    ||= {:gobo => 43}
+        options[:points][:teal]     ||= {:gobo => 50}
+        options[:points][:rings]    ||= {:gobo => 57}
 
         options[:capabilities] ||= [:pan, :tilt, :strobe, :gobo, :dimmer]
 
